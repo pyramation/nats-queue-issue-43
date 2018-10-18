43dd354606a94b717396d66f2e19226bec438a26
Dan Lynch <pyramation@gmail.com>
2018-10-18 15:35:09 -0700

    more specific location for crash comment

--- scripts/crash.sh
+++ scripts/crash.sh
@@ -20,10 +20,10 @@ done
 echo faas deploy -f stack.yml --gateway 127.0.0.1:31112
 faas deploy -f stack.yml --gateway 127.0.0.1:31112
 ## sometimes this deploy does not actually work since it could still be removing...
-## this is where the crash should start
 
 for i in {1..10};
 do
+  ## this is where the crash should start
   echo "calling async callback $i (redeploying)";
   curl -X POST 'http://localhost:31112/async-function/foo' -H 'X-Callback-Url:http://gateway.openfaas:8080/function/bar' --data '{"workerId":"worker-0.9176798083869617", "jobId":12}'
   sleep 2;
